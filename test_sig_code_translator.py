55a91a4c85a7a5e604710e9c62449d34516acdff
Bay40k <bay40k@gmail.com>
2022-02-04 14:58:22 -0700

    update unit tests

--- test_sig_code_translator.py
+++ test_sig_code_translator.py
@@ -7,37 +7,39 @@ class MyTestCase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         self.test_data = [
             {
-                "input": "take 1 daily by mouth for nausea and vomiting at night for pain in left eye",
-                "output": "tk 1 d po for nv noct for p in os",
+                "input": "take 1 tablet by mouth every day as needed for pain",
+                "output": "tk 1 t po qd prn p",
             },
             {
                 "input": "take 1 before meals and apply to affected area twice daily",
-                "output": "tk 1 ac & aaa bid",
+                "output": "tk 1 ac and aaa bid",
             },
             {
                 "input": "take 2 capsule daily in left ear twice daily as needed for anxiety",
                 "output": "tk 2 c d in as bid pra",
             },
             {
-                "input": "as needed as needed for as needed for anxiety as needed for",
-                "output": "prn prf pra prf",
+                "input": "as needed as needed for anxiety as needed for anxiety as needed as needed for anxiety",
+                "output": "prn pra pra prn pra",
             },
         ]
         super().__init__(*args, **kwargs)
 
-    def test_to_sig(self):
+    def test_from_sig(self):
         # test case and stripping
         self.assertEqual(
-            "AS NEEDED AS NEEDED FOR AS NEEDED FOR ANXIETY AS NEEDED FOR",
-            from_sig(" prn pRf Pra prf "),
+            "AS NEEDED AS NEEDED FOR ANXIETY AS NEEDED FOR ANXIETY AS NEEDED AS NEEDED FOR ANXIETY",
+            from_sig(" prn Pra pRa prn pra  "),
         )
         for test in self.test_data:
             self.assertEqual(test["input"].upper(), from_sig(test["output"]))
 
-    def test_from_sig(self):
+    def test_to_sig(self):
         self.assertEqual(
-            "PRN PRF PRA PRF",
-            to_sig(" as needed as neEdeD for as needed for anxiety as needed for  "),
+            "PRN PRA PRA PRN PRA",
+            to_sig(
+                " as needed as neeDed for anXiety as neEded for anxiety as needed as needed for anxiety  "
+            ),
         )
         for test in self.test_data:
             self.assertEqual(test["output"].upper(), to_sig(test["input"]))
